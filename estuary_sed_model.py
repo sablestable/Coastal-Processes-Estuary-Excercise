273486aa71d5f8a281364d1d00992cd52b81e9ca
sablestable <126003014+sablestable@users.noreply.github.com>
2024-09-13 18:06:36 -0500

    Add files via upload

---
+++ estuary_sed_model.py
@@ -0,0 +1,175 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 12:28:44 2024
+
+@author: murph
+"""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#define the annual sediment volume in m^3
+sed_vol = 10**6
+
+#define the total area of the estuary in m^2
+total_area = 1.5*10**8
+
+#restrict area to open water
+#define the open water area as owa of the estuary in m^2
+#this will be used in both elevation and sea level calculations
+owa = total_area*0.6
+
+#define the starting sea level
+slh = 1.6
+
+#Next we define the open water volume as omv in m^3
+#Expressed as 1.6 m depth x owa
+
+omv = owa*slh
+
+#now we have an expression of voolume, but this may be discarded as 
+#I believe that we can simplify to a 1-Dimensional model.
+
+#define subsidence rate subs
+subs = 0.003
+
+#define sea level rise slr
+slr = 0.002
+#set starting elevation at 0
+ele = 0
+
+#define temporal range of model
+years = range(1,52)
+year = 0
+#set up a dataframe for elevation of sediment and sea level
+#df = np.dataframe(ele)
+Estuary_basic = {'Sea Level': [1.6, slh],
+                        'Elevation': [0, ele],
+                        'Year' : [0, 1],
+                        }
+Estuary_basic_df = pd.DataFrame(Estuary_basic)
+Estuary_SLR1 = Estuary_basic_df
+Estuary_SLR2 = Estuary_basic_df
+Estuary_SLR3 = Estuary_basic_df
+
+ele_change = 0.8*sed_vol/owa
+    
+## Basic Model
+for elevation in years:
+        if year >= 50:
+            print('local sea level has not been overcome by sedimentary processes by the end of model runs.')
+            print(Estuary_basic_df)
+            break
+        elif ele > slh:
+            print("Elevation overcomes sea level at year: ", year)
+            print(Estuary_basic_df)
+            break
+        else:
+            year += 1
+            print("At year: ", year)
+            # accretion minus subsidence
+            ele += ele_change - subs
+            print('elevation = ' , ele, 'meters')
+            #increase sea level by sea level rise (unchanging)
+            slh += slr
+            df = pd.DataFrame({'Sea Level':[slh],
+                               'Elevation':[ele],
+                               'Year':[year]})
+            Estuary_basic_df = pd.concat([Estuary_basic_df, df], axis= 0)
+            print("sea level = ", slh, "meters")
+#reset values        
+ele = 0
+year = 0
+slh = 1.6
+slr = 0.002
+#Scenario with 0.02 SLR acceleration
+for elevation in years:
+        if year >= 50:
+            print('local sea level has not been overcome by sedimentary processes by the end of model runs.')
+            print(Estuary_SLR1)
+            break
+        elif ele > slh:
+            print("Elevation overcomes sea level at year: ", year)
+            print(Estuary_SLR1)
+            break
+        else:
+            year += 1
+            print("At year: ", year)
+            # accretion minus subsidence
+            ele += ele_change - subs
+            print('elevation = ' , ele, 'meters')
+            #increase sea level by sea level rise 
+            slr += slr*0.02
+            slh += slr
+            df = pd.DataFrame({'Sea Level':[slh],
+                               'Elevation':[ele],
+                               'Year':[year]})
+            Estuary_SLR1 = pd.concat([Estuary_SLR1, df], axis= 0)
+            print("sea level = ", slh, "meters")
+            
+#reset values
+ele= 0
+year = 0
+slh = 1.6
+slr = 0.002
+#Scenario with 0.05 SLR acceleration
+for elevation in years:
+        if year >= 50:
+            print('local sea level has not been overcome by sedimentary processes by the end of model runs.')
+            print(Estuary_SLR2)
+            break
+        elif ele > slh:
+            print("Elevation overcomes sea level at year: ", year)
+            print(Estuary_SLR2)
+            break
+        else:
+            year += 1
+            print("At year: ", year)
+            # accretion minus subsidence
+            ele += ele_change - subs
+            print('elevation = ' , ele, 'meters')
+            #increase sea level by sea level rise 
+            slr += slr*0.05
+            slh += slr
+            df = pd.DataFrame({'Sea Level':[slh],
+                               'Elevation':[ele],
+                               'Year':[year]})
+            Estuary_SLR2 = pd.concat([Estuary_SLR2, df], axis= 0)
+            print("sea level = ", slh, "meters")
+            
+#reset values
+ele = 0
+year = 0
+slh = 1.6
+slr = 0.002
+
+#Scenario with 0.07 SLR acceleration
+for elevation in years:
+        if year >= 50:
+            print('local sea level has not been overcome by sedimentary processes by the end of model runs.')
+            print(Estuary_SLR3)
+            break
+        elif ele > slh:
+            print("Elevation overcomes sea level at year: ", year)
+            print(Estuary_SLR3)
+            break
+        else:
+            year += 1
+            print("At year: ", year)
+            # accretion minus subsidence
+            ele += ele_change - subs
+            print('elevation = ' , ele, 'meters')
+            #increase sea level by sea level rise 
+            slr += slr*0.07
+            slh += slr
+            df = pd.DataFrame({'Sea Level':[slh],
+                               'Elevation':[ele],
+                               'Year':[year]})
+            Estuary_SLR3 = pd.concat([Estuary_SLR3, df], axis= 0)
+            print("sea level = ", slh, "meters")
+
+
+Scenario_1_plot = Estuary_basic_df.plot.line(x='Year', title = 'Scenario 1: No Acceleration to SLR')
+Scenario_2_plot = Estuary_SLR1.plot.line(x='Year', title = 'Scenario 2: 2% Acceleration in SLR')
+Scenario_3_plot = Estuary_SLR2.plot.line(x='Year', title = 'Scenario 3: 5% Acceleration in SLR')
+Scenario_4_plot = Estuary_SLR3.plot.line(x='Year', title = 'Scenario 4: 7% Acceleration in SLR')
